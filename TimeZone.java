8cfadeeea03fbca775a7bf03346362d7deeaa32b
Orlando <orlandojsjr@gmail.com>
2023-04-03 17:44:12 -0300

    fix typo

--- TimeZone.java
+++ TimeZone.java
@@ -2,6 +2,6 @@ import java.time.*;
 
 public class TimeZone {
   public static void main(String args[]) {
-    System.out.println(ZonedDatime.now(ZoneId.of("America/Mexico_City")).toString());
+    System.out.println(ZonedDateTime.now(ZoneId.of("America/Mexico_City")).toString());
   }
 }
