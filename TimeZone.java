041ecaf11e60d4e875f810faa073ae6f75c334bd
Orlando <orlandojsjr@gmail.com>
2023-04-03 18:21:58 -0300

    Update TimeZone.java

--- TimeZone.java
+++ TimeZone.java
@@ -2,6 +2,8 @@ import java.time.*;
 
 public class TimeZone {
   public static void main(String args[]) {
-    System.out.println(ZonedDateTime.now(ZoneId.of("America/Mexico_City")).toString());
+    System.out.println(String.format("MX %s", ZonedDateTime.now(ZoneId.of("America/Mexico_City")).toString()));
+    System.out.println(String.format("NY %s", ZonedDateTime.now(ZoneId.of("America/New_York")).toString()));
+    System.out.println(String.format("SP %s", ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).toString()));
   }
 }
